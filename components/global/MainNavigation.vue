6d2660b82c85d58a7ad2ffb04b69c4fb14006279
Emma Hons <emma.hons@student.luca-arts.be>
2024-11-17 14:56:07 +0100

    .

--- components/global/MainNavigation.vue
+++ components/global/MainNavigation.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="">
-    <ul 
-      v-for="post in folders"
-      :key="post.slug"
-      >
-      <NuxtLink :to="post._path" >
-          <li class="">{{ post.title }}</li>
-      </NuxtLink>
-    </ul>
-    <hr>
-    <NuxtLink to="about" >
-          about
-      </NuxtLink>
+    <el-collapse accordion class="accordion">
+      <el-collapse-item title="Work" name="1">
+        <ul v-for="post in folders" :key="post.slug">
+          <NuxtLink :to="post._path">
+            <li class="">{{ post.title }}</li>
+          </NuxtLink>
+        </ul>
+      </el-collapse-item>
+    </el-collapse>
+
+    <NuxtLink to="about">
+      about
+    </NuxtLink>
   </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import { useAsyncData } from '#app';
-  
-  // Define a ref for storing folders
-  const folders = ref([]);
-  
-  // Fetch all posts from both /folders and /page
-  const { data: foldersPosts } = await useAsyncData('folders', () => queryContent('/')
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useAsyncData } from '#app';
+
+// Define a ref for storing folders
+const folders = ref([]);
+
+// Fetch all posts from both /folders and /page
+const { data: foldersPosts } = await useAsyncData('folders', () => queryContent('/')
   .sort({ numbernavigation: 1 })
   .find());
 
-  // Combine and filter the data
-  onMounted(() => {
-    const allPosts = [...(foldersPosts.value || [])];
-    if (allPosts.length) {
-      folders.value = allPosts.filter(post => post.included && post.included === true && post.included !== false);
-    }
-  });
-  </script>
+// Combine and filter the data
+onMounted(() => {
+  const allPosts = [...(foldersPosts.value || [])];
+  if (allPosts.length) {
+    folders.value = allPosts.filter(post => post.included && post.included === true && post.included !== false);
+  }
+});
+</script>
